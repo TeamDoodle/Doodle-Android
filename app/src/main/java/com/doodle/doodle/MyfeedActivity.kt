98fbcf801477d608867fba4be855a6f08ab028b2
jaeleum <kjl0019@gmail.com>
2017-12-31 19:12:06 +0900

    12-31JJAM

---
+++ app/src/main/java/com/doodle/doodle/MyfeedActivity.kt
@@ -0,0 +1,12 @@
+package com.doodle.doodle
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MyfeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myfeed)
+    }
+}
